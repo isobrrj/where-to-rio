4c0ec06d636392ceb9eb3815a4f2637d11693035
Igor Soares <58219535+isobrrj@users.noreply.github.com>
2024-11-11 20:03:31 -0300

    feat: auth user (#2)

--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,19 @@
 import streamlit as st
+from utils.auth.auth_users import AuthUser
 
-st.write("Hello World")
+
+authenticator = AuthUser()
+
+login_status = authenticator.login()
+
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Bem Vindo *{st.session_state["name"]}*')
+    st.title('Página de Sistema')
+
+elif st.session_state["authentication_status"] is False:
+    st.error('Usuário/Senha is inválido')
+
+elif st.session_state["authentication_status"] is None:
+    st.warning('Por Favor, utilize seu usuário e senha!')
 
