bac03c068223e7ff5dbde218500879434b5b3053
iuryrosal <iuryrosal@gmail.com>
2024-11-20 16:40:00 -0300

    Ajusta main com o tourism preference

--- app/src/main.py
+++ app/src/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from utils.auth.auth_users import AuthUser
-
+from request_screen import RequestScreen
+from tourism_preference import TourismPreference
 
 authenticator = AuthUser()
 
@@ -9,7 +10,13 @@ login_status = authenticator.login()
 if st.session_state["authentication_status"]:
     authenticator.logout()
     st.write(f'Bem Vindo *{st.session_state["name"]}*')
-    st.title('Página de Sistema')
+    request_screen = RequestScreen()
+    init_date, end_date, neigh, preferences = request_screen.show_window()
+    tourism_preference = TourismPreference(neigh=neigh,
+                                            init_date=init_date,
+                                            end_date=end_date,
+                                            preferences=preferences)
+    print(tourism_preference.build_message())
 
 elif st.session_state["authentication_status"] is False:
     st.error('Usuário/Senha is inválido')
