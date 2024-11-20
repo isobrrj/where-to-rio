63e99e2a39535c56ab3127e94e4a2b3c6ca2d95a
iuryrosal <iuryrosal@gmail.com>
2024-11-20 18:17:26 -0300

    Remove metodo desnecessario do TourismPreference

--- app/src/tourism_preference.py
+++ app/src/tourism_preference.py
@@ -4,11 +4,3 @@ class TourismPreference:
         self.init_date = init_date
         self.end_date = end_date
         self.preferences = preferences
-
-    def build_message(self):
-        str_question = f"Quais pontos turisticos posso visitar, durante minha viagem, entre o dia {self.init_date} e {self.end_date} no Rio de Janeiro? Estarei hospedado no bairro {self.neigh} e tenho preferência por "
-        for k, v in self.preferences:
-            if self.preferences[k]:
-                str_question += f" {k} "
-        str_question += "."
-        return str_question
